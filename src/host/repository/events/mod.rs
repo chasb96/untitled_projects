13e56bfc8152038f695cbbf3b56e12810475fd59
Charles Bassett <chasb96@gmail.com>
2024-05-10 20:24:36 -0500

    Remove unused Event model

--- src/host/repository/events/mod.rs
+++ src/host/repository/events/mod.rs
@@ -1,34 +1,11 @@
 mod error;
 mod postgres;
 
-use chrono::{DateTime, Utc};
-use sqlx::postgres::PgRow;
-use sqlx::types::Json;
-use sqlx::Row;
-
 use crate::host::events::{EventKind, Snapshot};
 
 use self::error::QueryError;
 
 use super::postgres::PostgresDatabase;
-
-struct Event {
-    pub id: i32,
-    pub project_id: String,
-    pub content: EventKind,
-}
-
-impl From<PgRow> for Event {
-    fn from(value: PgRow) -> Self {
-        let content: Json<EventKind> = value.get("content");
-
-        Event {
-            id: value.get("id"),
-            project_id: value.get("project_id"),
-            content: content.0,
-        }
-    }
-}
 
 pub trait EventsRepository {
     async fn create(&self, project_id: &str, event: EventKind) -> Result<(), QueryError>;
