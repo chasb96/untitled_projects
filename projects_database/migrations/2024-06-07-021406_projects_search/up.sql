3d7377f95070ab9839608a26ec965ea62dd4173b
Charles Bassett <chasb96@gmail.com>
2024-06-06 21:25:57 -0500

    Fix missing column

--- projects_database/migrations/2024-06-07-021406_projects_search/up.sql
+++ projects_database/migrations/2024-06-07-021406_projects_search/up.sql
@@ -3,8 +3,9 @@ CREATE EXTENSION fuzzystrmatch;
 
 CREATE TABLE projects_search (
     code VARCHAR(32) NOT NULL,
+    value VARCHAR(32) NOT NULL,
     project_id VARCHAR(16) NOT NULL,
     name VARCHAR(32) NOT NULL
 );
 
-CREATE INDEX idx_projects_search_name_trgm ON projects_search USING gin (name gin_trgm_ops);
+CREATE INDEX idx_projects_search_value_trgm ON projects_search USING gin (value gin_trgm_ops);
